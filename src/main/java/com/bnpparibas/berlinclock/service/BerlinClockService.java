64ffd8ffffd4a044ee74e097970771e7d30769dc
Ozkan Ogus <ozkanogus@gmail.com>
2026-01-19 22:23:17 +0100

    TDD: add five hours row logic

--- src/main/java/com/bnpparibas/berlinclock/service/BerlinClockService.java
+++ src/main/java/com/bnpparibas/berlinclock/service/BerlinClockService.java
@@ -5,4 +5,13 @@ public class BerlinClockService {
     public String getSecondsLamp(int seconds) {
         return seconds % 2 == 0 ? "Y" : "0";
     }
+
+    public String getFiveHoursRow(int hours){
+        int onLamps = hours / 5;
+        StringBuilder row = new StringBuilder();
+        for(int i=0;i<4;i++){
+            row.append(i < onLamps ? "R" : "0");
+        }
+        return row.toString();
+    }
 }
--- src/test/java/com/bnpparibas/berlinclock/service/BerlinClockServiceTest.java
+++ src/test/java/com/bnpparibas/berlinclock/service/BerlinClockServiceTest.java
@@ -23,4 +23,30 @@ public class BerlinClockServiceTest {
         assertEquals("0",result);
     }
 
+    @Test
+    void shouldReturnAllOffForZeroHourInFiveHoursRow(){
+        BerlinClockService service = new BerlinClockService();
+
+        String result = service.getFiveHoursRow(0);
+
+        assertEquals("0000",result);
+    }
+
+    @Test
+    void shouldReturnTwoRedLampsForTenHour(){
+        BerlinClockService service = new BerlinClockService();
+
+        String result = service.getFiveHoursRow(10);
+
+        assertEquals("RR00",result);
+    }
+
+    @Test
+    void shouldReturnFourRedLampsForTwentyThreeHours(){
+        BerlinClockService service = new BerlinClockService();
+
+        String result = service.getFiveHoursRow(23);
+
+        assertEquals("RRRR",result);
+    }
 }
