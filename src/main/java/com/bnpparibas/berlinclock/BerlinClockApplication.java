7998e449c9edbc01128eaba69429db396b45e6d6
Ozkan OGUS <ozkanogus@gmail.com>
2026-01-19 13:21:42 +0100

    Initial Spring Boot project skeleton with Gradle and Java 21

---
+++ src/main/java/com/bnpparibas/berlinclock/BerlinClockApplication.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.berlinclock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerlinClockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerlinClockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnpparibas/berlinclock/BerlinClockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpparibas.berlinclock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerlinClockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
