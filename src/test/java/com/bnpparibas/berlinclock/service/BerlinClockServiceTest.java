b330111e84592b6e3296dce34c497897e66e6478
Ozkan Ogus <ozkanogus@gmail.com>
2026-01-20 01:33:40 +0100

    Add edge case tests, the last second and midnight

--- src/test/java/com/bnpparibas/berlinclock/service/BerlinClockServiceTest.java
+++ src/test/java/com/bnpparibas/berlinclock/service/BerlinClockServiceTest.java
@@ -16,5 +16,20 @@ public class BerlinClockServiceTest {
        assertEquals("YYYY",service.getOneMinuteRow(59));
     }
 
+    @Test
+    void testEdgeCases(){
+        // Midnight
+        assertEquals("Y",service.getSecondsLamp(0));
+        assertEquals("0000",service.getFiveHoursRow(0));
+        assertEquals("0000",service.getOneHourRow(0));
+        assertEquals("00000000000",service.getFiveMinutesRow(0));
+        assertEquals("0000",service.getOneMinuteRow(0));
 
+        // Last second
+        assertEquals("0",service.getSecondsLamp(59));
+        assertEquals("RRRR",service.getFiveHoursRow(23));
+        assertEquals("RRR0",service.getOneHourRow(23));
+        assertEquals("YYRYYRYYRYY",service.getFiveMinutesRow(59));
+        assertEquals("YYYY",service.getOneMinuteRow(59));
+    }
 }
